c9db14a5290d9d41faad5c98e6f498380172ef6d
Arek <kontakt@ohsofresh.pl>
2026-09-10 16:06:05 +0200

    Automatyczny deploy

--- resources/views/woocommerce/global/quantity-input.blade.php
+++ resources/views/woocommerce/global/quantity-input.blade.php
@@ -25,22 +25,23 @@ $is_hidden = ($max_value && $min_value === $max_value);
         min: {{ $min }}, 
         max: {{ $max }}, 
         step: {{ $step }},
-        decrement() { if (this.value > this.min) this.value = Math.max(this.min, this.value - this.step) },
-        increment() { if (this.value < this.max) this.value = Math.min(this.max, this.value + this.step) }
+        decrement() { if (this.value > this.min) this.value = Math.max(this.min, this.value - this.step); $refs.qtyInput.dispatchEvent(new Event('change', { bubbles: true })) },
+        increment() { if (this.value < this.max) this.value = Math.min(this.max, this.value + this.step); $refs.qtyInput.dispatchEvent(new Event('change', { bubbles: true })) }
     }">
         <div class="flex items-center border-1 border-secondary overflow-hidden radius w-fit h-[66px] select-none bg-white">
-            
+
             {{-- Przycisk Minus --}}
-            <button type="button" 
-                @click="decrement()" 
+            <button type="button"
+                @click="decrement()"
                 class="px-4 py-2 hover:bg-secondary-50 transition-colors text-primary font-bold text-lg leading-none cursor-pointer h-full flex items-center justify-center border-0 bg-transparent min-w-[40px]">
                 −
             </button>
-            
+
             {{-- Standardowy Input WooCommerce (schowane strzałki) --}}
             <input
                 type="number"
                 id="{{ esc_attr($input_id) }}"
+                x-ref="qtyInput"
                 class="qty !m-0 !p-0 !border-0 text-center w-12 h-10 border-x border-secondary focus:outline-none focus:ring-0 text-primary-500 font-semibold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 step="{{ esc_attr($step) }}"
                 min="{{ esc_attr($min_value) }}"
@@ -49,10 +50,10 @@ $is_hidden = ($max_value && $min_value === $max_value);
                 x-model.number="value"
                 @input="value = Math.min(max, Math.max(min, parseInt($event.target.value) || min))"
             />
-            
+
             {{-- Przycisk Plus --}}
-            <button type="button" 
-                @click="increment()" 
+            <button type="button"
+                @click="increment()"
                 class="px-4 py-2 hover:bg-secondary-50 transition-colors text-primary font-bold text-lg leading-none cursor-pointer h-full flex items-center justify-center border-0 bg-transparent min-w-[40px]">
                 +
             </button>
